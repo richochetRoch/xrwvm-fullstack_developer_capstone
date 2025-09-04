d7c82157c3a053a78c5a39868ebbba7048dcc9d3
richochetRoch <steverochester1@gmail.com>
2025-09-04 12:01:48 +0100

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,7 @@ urlpatterns = [
     # path for login
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
-    path(route='register', view=views.registration, name='registration')
+    path(route='register', view=views.registration, name='registration'),
     path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
     path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
